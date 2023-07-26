dc6e335d97f485bf9a327e583ba8da8c4d71199c
strenadkosa <strenadkosa@mail.ru>
2023-07-26 18:20:31 +0300

    Решение задач по семинару №6

---
+++ first/venv_1/Seminar_6/1_task.py
@@ -0,0 +1,11 @@
+# ------------------------------------------- 1 -----------------------------
+# Вспомните какие модули вы уже проходили на курсе.
+# Создайте файл, в котором вы импортируете встроенные
+# в модуль функции под псевдонимами. (3-7 строк импорта).
+
+
+from decimal import Decimal as D
+from random import randint as rndi
+from math import sqrt as square_root
+from sys import argv as arguments
+from fractions import Fraction as F
